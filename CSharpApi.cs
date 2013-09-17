20c9de4906b14f350211ddd1a9c6d23ff70792ce
Abhiranjan Kumar <abhiranjan.kumar00@gmail.com>
2013-09-17 15:00:47 +0530

    Added files

---
+++ CSharpApi.cs
@@ -0,0 +1,326 @@
+using System;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+public class Agile: ICertificatePolicy 
+{
+  /*******Please insert your Domain Name and Api Key here*********/
+  const string domain = "xxx";
+  const string apiKey = "xxx";
+  /***************************************************************/ 
+
+  const string  domainUrl = "https://" + domain + ".agilecrm.com/core/js/api/";
+
+  /*
+   * If no such contact found, returns a json string where value of 'id' key maps to 'null'.
+   */
+  static public string GetContact(string email) 
+  {
+    string nextUrl = "contact/email/";
+    string queryString = "email=" +email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  } 
+
+  /*
+   * It creates a contact with description 'contactJson'.
+   */
+  static public string CreateContact(string contactJson) 
+  {
+    string nextUrl = "contacts/";
+    string queryString = "contact=" + contactJson;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns null if no contact is found otherwise a success status.
+   */
+  static public string DeleteContact(string email) 
+  {
+    string nextUrl = "contact/delete/";
+    string queryString = "email=" +email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Create a task represented by 'taskJson', in stringified json format, for contact associated with 'email'.
+   */
+  static public string CreateTask(string email, string taskJson) 
+  {
+    string nextUrl = "task/";
+    string queryString = "email=" + email + "&task=" + taskJson;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add deal and relate contact to the deal 'opportunity', in stringified json format.
+   */
+  static public string CreateDeals(string email, string deal) 
+  {
+    string nextUrl = "opportunity/";
+    string queryString = "email=" + email + "&opportunity=" + deal;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add/replace 'property', in stringified json format, with the contact associated with 'email'.
+   */
+  static public string AddProperty(string email, string property) 
+  {
+    string nextUrl = "contacts/add-property/";
+    string queryString = "email=" + email + "&data=" + property;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add 'note', in stringified json format, to the contact associated with 'email'
+   */
+  static public string AddNote(string email, string note) 
+  {
+    string nextUrl = "contacts/add-note/";
+    string queryString = "email=" + email + "&data=" + note;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add 'campaign', in stringified json format, to the contact associated 'email'.
+   */
+  static public string AddCampaign(string email, string campaign) 
+  {
+    string nextUrl = "contacts/add-campaign/";
+    string queryString = "email=" + email + "&data=" + campaign;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Get template of gadget 'gadget'.
+   */
+  static public string GetGadgetTemplate(string gadget) 
+  {
+    string nextUrl = "gmail-template/";
+    string queryString = "template=" + gadget;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add 'score' to the score of contact associated with 'email' email. It always returns null.
+   */
+  static public string AddScore( string email, int score) 
+  {
+    string nextUrl = "contacts/add-score/";
+    string queryString = "email=" +email + "&score=" + score;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Subtract 'score' from the score of contact associated with 'email' email. It always returns null.
+   */
+  static public string SubtractScore( string email, int score) 
+  {
+    string nextUrl = "contacts/subtract-score/";
+    string queryString = "email=" +email + "&score=" + score;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Add tags mentioned in 'tags' to the contact with email 'email'. If tag is already present it returns the contact in stringified json object.
+   */
+  static public string AddTags(string email, string tags) 
+  {
+    string nextUrl = "contacts/add-tags/";
+    string queryString = "email=" + email + "&tags=" +tags;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Remove tags mentioned in 'tags' to the contact with email 'email'. If tag is already absent, it returns contact in stringified json object.
+   */
+  static public string RemoveTags(string email, string tags) 
+  {
+    string nextUrl = "contacts/remove-tags/";
+    string queryString = "email=" + email + "&tags=" +tags;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Enrolls a contact with contact id "contactId" to workflow with id "workflowId"
+   */
+  // ** It will return some EXCEPTION, IGNORE this.
+  static public string SubscribeContact(long contactId, long workflowId) 
+  {
+    string nextUrl = "campaign/enroll/" + contactId + "/" + workflowId+"/";
+    string queryString = "";
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns the score of contact with email 'email'. If there's no such contact returns a null object.
+   */
+  static public string GetScore(string email) 
+  {
+    string nextUrl = "contacts/get-score/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns the notes in sringified json object form of contact with email 'email'. If there's no such contact returns a null object.
+   */
+  static public string GetNotes(string email) 
+  {
+    string nextUrl = "contacts/get-notes/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns the tags of contact with email 'email'. If there's no such contact returns a null object. Result will be stringified form of array of strings, eg., "[\"tag1\", \"tag2\"]"
+   */
+  static public string GetTags(string email) 
+  {
+    string nextUrl = "contacts/get-tags/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns the tasks of contact with email 'email'. If there's no such contact returns a null object.
+   */
+  static public string GetTasks(string email) 
+  {
+    string nextUrl = "contacts/get-tasks/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns the deals with contact with email 'email'. If there's no such contact returns a null object.
+   */
+  static public string GetDeals(string email) 
+  {
+    string nextUrl = "contacts/get-deals/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns all the detail of the campaings with which contact with 'emai' is associated.
+   */
+  static public string GetCampaigns(string email) 
+  {
+    string nextUrl = "contacts/get-campaigns/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns all log of the campaings with which contact with 'emai' is associated.
+   */
+  static public string GetCampaignLogs(string email) 
+  {
+    string nextUrl = "contacts/get-campaign-logs/";
+    string queryString = "email=" + email;
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /*
+   * Returns all the work flow of current domain user. Result will be stringified form of array of strings.
+   */
+  static public string GetWorkflows() 
+  {
+    string nextUrl = "contacts/get-workflows/";
+    string queryString = "";
+
+    string result = HttpGet(nextUrl, queryString);
+    return result;
+  }
+
+  /************************PRIVATE****************************************/
+
+  private static string HttpGet(string nextUrl, string queryString) 
+  {
+    try 
+    {
+      string url = domainUrl + nextUrl + "?" + "id=" + apiKey; 
+
+      if(!string.IsNullOrEmpty(queryString))
+        url += "&" + queryString;
+
+      // url = System.Web.HttpUtility.UrlEncode(url);   // Library not provided everywhere, at least at mono.
+      //url = System.Uri.EscapeUriString(url);          // Should not be used for encoding whole url.
+      url = System.Uri.EscapeDataString(url);
+
+      //Console.WriteLine(url);
+
+      HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+      request.Method = "GET";
+      string result = null;
+
+      using(HttpWebResponse response = request.GetResponse() as HttpWebResponse) 
+      {
+        Stream dataStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(dataStream);
+        result = reader.ReadToEnd();
+
+        reader.Close();
+        dataStream.Close();
+        response.Close();
+      }
+
+      return result;
+    }
+    catch (Exception e)
+    {
+      return "Exception caught!!!\n" + e.ToString();
+    }
+  }
+
+  public bool CheckValidationResult(ServicePoint sp, X509Certificate certificate, WebRequest request, int error) 
+  {
+    return true;
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,178 @@
+Agile CRM C# API
+================
+Agile CRM is a new breed CRM. You can sign up @ [AgileCRM.com](https://www.agilecrm.com).  
+
+Requirements
+------------
+C# 3.0 with .NET 3.5 or later.
+
+BASICS
+------
+
+* Please enter you Api Key and Domain name at begining of the definition of `class Agile`.
+
+* If you login at `https://xyz.agilecrm.com` then your domain name is `xyz`.
+
+* Your api key is present at the "Api & Analytics" tab at `https://xyz.agilecrm.com/#account-prefs`.
+
+* All methods are static.
+
+* Here `email` is a string, representing a *email id*,  which is used to refer a specific contact.
+
+* All method will return a string object which will be a json stringified form. 
+
+* For representation of complex data, we used stringified json format.
+
+
+API's Details
+-------------
+###1. Contact 
+#####1.1 `string Agile.GetContact(string email)`  
+This is used to retrieve the contact details of a person. It will return in json format.
+
+#####1.2 `string Agile.CreateContact(string contactJson)`  
+This is used to create a new contact with properties mentioned in `contactJson`.
+
+#####1.3 `string Agile.DeleteContact(string email)`  
+This is used to delete contact details of the person.
+
+###2. Property
+#####2.1 `string Agile.AddProperty(string email, string property)`  
+This is used to add/modify the property of a contact. Here `property` is a json object containing property data and value to be added.
+
+###3. Score
+#####3.1 `string Agile.GetScore(string email)`  
+This is to retrieve the score of a contact.
+
+#####3.2 `string Agile.AddScore( string email, int score)`  
+This is to increment score of a contact by `score` units.
+
+#####3.3 `string Agile.SubtractScore( string email, int score)`  
+This is to decrement score of a contact by `score` units.
+
+###4. Notes
+#####4.1 `string Agile.GetNotes(string email)`   
+This is to retrieve notes of a contact.
+
+#####4.2 `string Agile.AddNote(string email, string note)`   
+This is used to add a note represented with a stringified json object to a contact. Here `note` is a json object containing the subject and description field of the note.
+
+###5. Tags
+#####5.1 `string Agile.GetTags(string email)`   
+This is to retrieve tags of a contact.
+
+#####5.2 `string Agile.AddTags(string email, string tags)`   
+This is to add tags to a contact. Here,   
+
+    string tags = "tag1, tag2, tag3";
+         (or) 
+    string tags = "tag1 tag2 tag3";
+
+#####5.3 `string Agile.RemoveTags(string email, string tags)`   
+This is to remove tags referred by `tags` from a contact. Here,   
+
+    string tags = "tag1, tag2, tag3"; 
+        (or)
+    string tags = "tag1 tag2 tag3";
+
+###6. Tasks
+#####6.1 `string Agile.GetTasks(string email)`   
+This is to retrieve tasks of a contact.
+
+#####6.2 `string Agile.CreateTask(string email, string taskJson)`   
+This is used to add a task with property mentioned in `taskJson` to a contact. Here task is passed in a stringified json format.
+
+###7. Deals
+#####7.1 `string Agile.GetDeals(string email)`   
+This is to retrieve all the deals of a contact.
+
+#####7.2 `string Agile.CreateDeals(string email, string deal)`   
+This is used to add detail about a deal referred by `deal` to a contact. Here 'opportunity' is a strinfied json object, where original json object is in a form of :  
+    
+    {   
+    "name": "Deal sales", "description": "brief description on deal",   
+    "expected_value": "100", "milestone":"won",    
+    "close_date": data as epoch time   
+    }
+
+###8. Campaigns
+#####8.1 `string Agile.GetCampaigns(string email)`   
+This is to retrieve details of all campaigns associated with a contact.
+
+#####8.2 `string Agile.AddCampaign(string email, string campaign)`   
+This is used to add campaign based on contact email to already existing workflow. 
+
+#####8.3 `string Agile.GetCampaignLogs(string email)`   
+This is to retrieve logs of all campaings associated with a contact.
+
+###9. Contact & Workflow
+#####9.1 `string Agile.SubscribeContact(long contactId, long workflowId)`   
+Enrolls a contact with `contactId` to a particular workflow with `workflowId`.
+
+###10. Workflows
+#####10.1 `string Agile.GetWorkflows()`   
+This is to retrieve the workflow of the user.
+
+###11. Gadget Templates
+#####11.1 `string Agile.GetGadgetTemplate(string gadget)`   
+This is used to return the template of the gadget with name `gadget`.
+
+Examples
+--------
+
+    //Fetching Contact detail from email in stringified json format.
+    string result = Agile.GetContact("jobs@apple.com");
+    
+    
+    //Deleting a contact by email.
+    string email = "dupemail@outlook.com";
+    result = Agile.DeleteContact(email);
+
+
+    //Adding Score
+    result = Agile.AddScore("jobs@apple.com", 12);
+
+
+    //Subtracting Score
+    result = Agile.SubtractScore ("jobs@apple.com", 12);
+
+
+    //Adding tag.
+    result = Agile.AddTags("jobs@apple.com", "hihoho");
+
+
+    //Removing tag.
+    result = Agile.RemoveTags("jobs@apple.com", "hohoho");
+
+
+    //Subscribe client with id 2001 to campaign #10001.
+    result = Agile.SubscribeContact(2001,10001);
+
+
+    //Get score of contact associated with 'email'.
+    result = Agile.GetScore("jobs@apple.com");
+
+
+    //Get all notes associated with 'email'.
+    result = Agile.GetNotes("jobs@apple.com");
+
+
+    //Get all tags associated with 'email'.
+    result = Agile.GetTags("jobs@apple.com");
+
+
+    //Get all tasks associated with 'email'.
+    result = Agile.GetTasks("jobs@apple.com");
+
+
+    //Get all campaigns associated with 'email'.
+    result = Agile.GetCampaigns("jobs@apple.com");
+
+
+    //Get all log of campaigns associated with 'email'.
+    result = Agile.GetCampaignsLogs("jobs@apple.com");
+
+
+    //Get all workflow of the domain.
+    result = Agile.GetWorkflows();
+
