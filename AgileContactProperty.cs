a50a340031f8be9964954a41f08c8eecf9df7d65
craigkitterman <craig@kitterman.net>
2015-03-25 12:48:40 -0700

    dependant objects

---
+++ AgileContactProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileCRM
+{
+    public class AgileContactProperty
+    {
+        public AgileContactProperty()
+        {
+            this.type = "SYSTEM";
+        }
+
+        public string type;
+        public string subtype { get; set; }
+
+        public string name { get; set; }
+
+        public string value { get; set; }
+    }
+}
